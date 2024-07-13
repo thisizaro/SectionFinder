9183169bcde07d0cd161497f3ce678f7d77f50d2
thisizaro <thisisaro.official@gmail.com>
2024-07-14 00:38:02 +0530

    port

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
+
+console.log("Starting server...");
 
 app.get("/", (req, res) => {
+  console.log("Received request at root path");
   res.send("Hello World!");
 });
 

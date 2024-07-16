77a7f2dce08574d39f5ad9fd8438e4f42715a663
thisizaro <thisisaro.official@gmail.com>
2024-07-17 02:42:42 +0530

    ...

--- Client/src/components/HomePage.jsx
+++ Client/src/components/HomePage.jsx
@@ -9,8 +9,8 @@ function HomePage() {
     <div className="App">
       <h1>Applicants</h1>
       <ApplicantTable></ApplicantTable>
-      <h1>Semester 3 Section 2024</h1>
-      <Table></Table>
+      {/* <h1>Semester 3 Section 2024</h1> */}
+      {/* <Table></Table> */}
     </div>
   );
 }
